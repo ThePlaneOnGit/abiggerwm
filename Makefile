d0fd783355df9173284511aa75d3b4b2a338d673
ThePlaneOnGit <deplenguy@proton.me>
2026-04-16 14:59:46 +0400

    little edits [NO NEW WINDOWS CAN BE CREATED, DO NOT USE]

--- Makefile
+++ Makefile
@@ -1,10 +1,21 @@
+# vim: set tabstop=6 noexpandtab shiftwidth=6 softtabstop=0 colorcolumn=100 ft=make: syntax on:
+# -*- mode: c indent-tabs-mode: t tab-width: 6 fill-column: 100 -*-
 TARGET = abiggerwm
 SRC = src/main.c
+INCLUDES = -I./keybinds -I./include -lX11
+WARN_FLAGS ?= -Wall -Wextra
+COMPILATION_FLAGS ?= -ggdb -O3
+CC ?= cc
+DEBUG ?=
 
 all: $(TARGET)
 
 $(TARGET): $(SRC)
-	gcc -Wall -Wextra -I./keybinds -I./include -g -O2 -o $(TARGET) $(SRC) -lX11
+	@test -f "keybinds/keybinds.h" ||\
+		cat keybinds/keybinds.h.example > keybinds/keybinds.h
+	@test -f "keybinds/keybind_wrappers.c" ||\
+	       	cat keybinds/keybind_wrappers.c.example > keybinds/keybind_wrappers.c
+	$(CC) -o $(TARGET) $(SRC) $(WARN_FLAGS) $(INCLUDES) $(COMPILATION_FLAGS) $(DEBUG)
 
 clean:
 	rm -f $(TARGET)
--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
-/* 
+/* vim: set tabstop=6 noexpandtab shiftwidth=6 softtabstop=0 colorcolumn=100 ft=c syntax=c:
+syntax on:
+*/
+// -*- mode: c indent-tabs-mode: t tab-width: 6 fill-column: 100 -*-
+/*
 ABiggerWM: A Window Manager That Adds A Few Cool Things
 Copyright (C) 2026 ANNONYMUS PERSON
 
@@ -15,54 +19,69 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 #include <X11/Xlib.h>
 #include "../keybinds/keybinds.h"
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define SANITIZED(masks) (masks & (Mod1Mask | Mod4Mask | Mod5Mask | ControlMask | ShiftMask))
+
+struct _Windows {
+	size_t max;
+	size_t used;
+	Window* data;
+};
+typedef struct _Windows Windows;
+
 Display* dpy;
 XWindowAttributes attr;
 XButtonEvent start;
 XEvent ev;
 Window focused_win = None;
+Windows windows;
 
-void focus_window(Window win) {
-    if (focused_win != None && focused_win != win) {
-        XSetWindowBorderWidth(dpy, focused_win, 1);
-    }
-    
-    if (win == None) {
-        focused_win = None;
-        return;
-    }
-
-    XRaiseWindow(dpy, win);
-    XSetInputFocus(dpy, win, RevertToParent, CurrentTime);
-    XSetWindowBorderWidth(dpy, win, 3);
-
-    focused_win = win;
-    XFlush(dpy);
-}
-
-void setup(void){
-	if (!(dpy = XOpenDisplay(0x0))) exit(1);
-
-	for (int i = 0; keybinds[i].key != NULL; i++)
-		XGrabKey(dpy, XKeysymToKeycode(dpy, XStringToKeysym(keybinds[i].key)), SANITIZED(keybinds[i].masks), DefaultRootWindow(dpy),
-				True, GrabModeAsync, GrabModeAsync);
-
-	for (int i = 0; mouse_events[i].button != (long long) NULL; i++)
-		XGrabButton(dpy, mouse_events[i].button, SANITIZED(mouse_events[i].masks), DefaultRootWindow(dpy),
-				True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask, GrabModeAsync, GrabModeAsync, None, None);
-
-	start.subwindow = None;
-}
-
-void mouse(XButtonEvent* start, XEvent* ev){
-	if (ev->type == ButtonRelease || start->subwindow == None){
-		start->subwindow = None;
+void focus_window(Display* dpy, Window win) {
+	if (win == None) {
+		focused_win = None;
 		return;
 	}
+
+	if (focused_win != None && focused_win != win) {
+		XSetWindowBorderWidth(dpy, focused_win, 1);
+	}
+
+	XRaiseWindow(dpy, win);
+	XSetInputFocus(dpy, win, RevertToParent, CurrentTime);
+	XSetWindowBorderWidth(dpy, win, 3);
+
+	focused_win = win;
+	XFlush(dpy);
+}
+
+void add_window(Display* dpy, Windows* windows, Window window){
+	if (windows->max == windows->used){
+		windows->max *= 2;
+		windows->data = realloc(windows->data, windows->max);
+	}
+	windows->used++;
+	windows->data[windows->used] = window;
+	XMapWindow(dpy, window);
+	focus_window(dpy, window);
+}
+
+void remove_window(Display* dpy, Windows* windows, Window window){
+	XUnmapWindow(dpy, window);
+	for (size_t i = 0; i < windows->used; i++)
+		if (windows->data[i] == window){
+			for (size_t j = i; j < windows->used; j++)
+				windows->data[j] = windows->data[j + 1];
+			break;
+		}
+}
+
+void mouse(Display* dpy, XButtonEvent* start, XEvent* ev){
+	if (start->subwindow == None) return;
+	focus_window(dpy, start->subwindow);
 
 	for (int i = 0; mouse_events[i].button != (long long) NULL; i++){
 		if ((start->button == mouse_events[i].button) &&
@@ -75,36 +94,73 @@ void mouse(XButtonEvent* start, XEvent* ev){
 
 }
 
-void keybd(XWindowAttributes* attr, XButtonEvent* start, XEvent* ev){
+void keybd(Display* dpy, XWindowAttributes* attr, XButtonEvent* start, XEvent* ev){
 	if (ev->xkey.subwindow == None) return;
 	if (ev->type == ButtonPress && ev->xbutton.subwindow != None) {
 		XGetWindowAttributes(dpy, ev->xbutton.subwindow, attr);
-        *start = ev->xbutton;
+		*start = ev->xbutton;
 
-        focus_window(ev->xbutton.subwindow);
+		focus_window(dpy, ev->xbutton.subwindow);
 		return;
 	}
 
 	if (ev->type != KeyPress ) return;
 	for (int i = 0; keybinds[i].key != NULL; i++){
 		KeyCode keycode = XKeysymToKeycode(dpy, XStringToKeysym(keybinds[i].key));
-        if (keycode && (ev->xkey.keycode == keycode) &&
+		if (keycode && (ev->xkey.keycode == keycode) &&
 				((SANITIZED(ev->xkey.state) == keybinds[i].masks))){
 			(*keybinds[i].function)();
 			return;
 		}
-	};
+	}
+	return;
+}
+
+void setup(Display** dpy, XButtonEvent* start, Window* focused_win){
+	if (!(*dpy = XOpenDisplay(0x0))) exit(1);
+	XSelectInput(*dpy, DefaultRootWindow(*dpy),
+			SubstructureRedirectMask | SubstructureNotifyMask);
+
+	for (int i = 0; keybinds[i].key != NULL; i++)
+		XGrabKey(*dpy, XKeysymToKeycode(*dpy, XStringToKeysym(keybinds[i].key)),
+			SANITIZED(keybinds[i].masks), DefaultRootWindow(*dpy),
+			True, GrabModeAsync, GrabModeAsync);
+
+	for (int i = 0; mouse_events[i].button != (long long) NULL; i++)
+		XGrabButton(*dpy, mouse_events[i].button,
+				SANITIZED(mouse_events[i].masks), DefaultRootWindow(*dpy),
+				True, ButtonPressMask|ButtonReleaseMask|PointerMotionMask,
+				GrabModeAsync, GrabModeAsync, None, None);
+	XSync(*dpy, False);
+	*focused_win     = None;
+	start->subwindow = None;
 	return;
 }
 
 int main(void) {
-	setup();
+	setup(&dpy, &start, &focused_win);
 	for (;;) {
 		XNextEvent(dpy, &ev);
 		waitpid(-1, NULL, WNOHANG);
-		if (ev.type == MotionNotify || ev.type == ButtonRelease)
-			mouse(&start, &ev);
-		else if (ev.type == ButtonPress || ev.type == KeyPress)
-			keybd(&attr, &start, &ev);
+		switch (ev.type){
+			case MotionNotify:
+				mouse(dpy, &start, &ev); 
+				break;
+			case ButtonRelease:
+				mouse(dpy, &start, &ev); 
+				break;
+			case ButtonPress: 
+				keybd(dpy, &attr, &start, &ev); 
+				break;
+			case KeyPress:
+				keybd(dpy, &attr, &start, &ev); 
+				break;
+			case MapRequest:
+				add_window(dpy, &windows, ev.xmaprequest.window); 
+				break;
+			case DestroyNotify:
+				remove_window(dpy, &windows, ev.xdestroywindow.window); 
+				break;
+		}
 	}
 }
